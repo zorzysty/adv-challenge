87707a3b5a95345a7cb0461cc9b812070cd52fec
zorza <zorza2@gmail.com>
2021-07-10 15:03:20 +0200

    initial project configuration

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "react-app",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/prop-types": ["off"],
+    "no-console": ["warn"],
+    "no-debugger": ["warn"],
+    "@typescript-eslint/no-empty-function": ["off"],
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
+      { "ts-ignore": "allow-with-description" },
+    ],
+    "import/no-default-export": ["error"],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external", "internal"],
+          "parent",
+          ["sibling", "index"],
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "import/newline-after-import": ["error"],
+    "import/no-absolute-path": ["error"],
+  },
+  overrides: [
+    {
+      // no need to set a return type of react components: https://kentcdodds.com/blog/how-to-write-a-react-component-in-typescript
+      files: ["src/**/*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": ["off"],
+      },
+    },
+  ],
+}
